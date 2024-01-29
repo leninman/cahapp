362077538a20950c207da34f8324421e2ee7a97b
leninman <leninmanrique@gmail.com>
2024-01-29 08:46:12 -0400

    Actualizacion a Angular 17 Agregado de comoponente login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { HomeComponent } from './pages/home/home.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { NavComponent } from './shared/nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    FooterComponent,
+    HeaderComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
